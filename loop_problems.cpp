21c6fcfb65174ced98103840e8b91a6592970e67
Harshit Maurya <harshitmaurya2006@gmail.com>
2026-06-05 18:00:19 +0000

    Add loop_problems.cpp and Untitled-1

---
+++ loop_problems.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int i,n;
+    cout<<"Enter the table :";
+    cin>>n;
+
+    for(i=1;i<=10;i++)
+    {
+        cout<<n<<"*"<<i<<"="<<n*i<<endl;
+    }
+}
